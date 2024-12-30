3400019a9947e1212c7151820089882ae6854c11
Daniel Martínez <skaitdan@gmail.com>
2024-12-30 13:01:43 +0100

    General config changes

--- configuration/projectSettings.py
+++ configuration/projectSettings.py
@@ -1,2 +1,5 @@
-IMAGES_PATH = 'data/images'
-IMAGE_FEATURES_PATH = 'data/image_embeddings'
+from pathlib import Path
+
+ENV_FILE_PATH = Path(__file__).resolve().parent.parent.joinpath('.env')
+IMAGES_PATH = Path(__file__).resolve().parent.parent.joinpath('data/images')
+IMAGE_FEATURES_PATH = Path(__file__).resolve().parent.parent.joinpath('data/image_embeddings')

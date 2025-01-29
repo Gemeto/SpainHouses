e1c036c4c21c8d3a2d8bc102a65363259c11f9de
Gemeto <skaitdan@gmail.com>
2025-01-30 00:15:43 +0100

    Code cleaning

--- web/SpainHouses/run.py
+++ web/SpainHouses/run.py
@@ -5,6 +5,6 @@ import os
 if __name__ == "__main__":
     app = create_app()
     app.config['DEBUG'] = True
-    app.config['MONGO_URI'] = f"mongodb://root:root@localhost/spainhouses?authSource=admin"
+    app.config['MONGO_URI'] = f"mongodb://{os.getenv('MONGO_USER', 'root')}:{os.getenv('MONGO_PASS', 'root')}@{os.getenv('MONGO_HOST', 'localhost')}/{os.getenv('MONGO_DB', 'spainhouses')}?authSource=admin"
 
     app.run(host='0.0.0.0')
